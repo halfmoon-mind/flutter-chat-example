a88b74e9f65ea23488c3dbf3aad42006c3b23dd7
Sanghyeon Sim <5338095@gmail.com>
2023-09-20 16:30:02 +0900

    📦 add linting packages

--- node-chat-server/src/index.ts
+++ node-chat-server/src/index.ts
@@ -1,32 +1,3 @@
-// import express from 'express';
-// import http from 'http';
-// import { Server } from 'socket.io';
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server);
-
-// io.on('connection', (socket) => {
-//   console.log('User connected');
-
-//   socket.on('set_username', (username) => {
-//     io.emit('set_username', `${username} has joined the chat`);
-//     console.log(`${username} has connected`);
-//   });
-
-//   socket.on('send_message', (message: string) => {
-//     io.emit('receive_message', message);
-//     console.log(message);
-//   });
-
-//   socket.on('disconnect', () => {
-//     console.log('User disconnected');
-//   });
-// });
-
-// server.listen(3000, () => {
-//   console.log('Server is running on port 3000');
-// });
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
