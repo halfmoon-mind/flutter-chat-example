3150dfa4a04e022567c06627be04ae77ea638799
Sanghyeon Sim <5338095@gmail.com>
2023-10-03 16:54:02 +0900

    🔒️ update CORS code

--- node-chat-server/src/index.ts
+++ node-chat-server/src/index.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import http from 'http';
 import * as controller from './apis';
 import { Server } from 'socket.io';
+
 import { socketService } from './socket';
 import Room from './model/room';
 
@@ -9,6 +10,15 @@ const app = express();
 export const server = http.createServer(app);
 export const roomList: Room[] = [];
 export const io = new Server(server);
+const cors = require('cors');
+
+app.use(
+  cors({
+    origin: 'https://lierchat.net', // 클라이언트 도메인
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
